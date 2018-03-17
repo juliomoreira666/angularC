bdfa17678cd2536fe7450f084ab4573a33294ad5
Julio Moreira <julio.moreira666@gmail.com>
2018-03-17 16:14:05 -0300

    NgFor

---
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.css']
+})
+export class CabecalhoComponent {
+  @Input() titulo;
+}
---
+++ src/app/components/cabecalho/cabecalho.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CabecalhoComponent} from './cabecalho.component';
+
+@NgModule ({
+    declarations: [
+        CabecalhoComponent
+      ],
+      exports: [
+        CabecalhoComponent
+      ]
+})
+export class CabecalhoModule {
+
+}
